feaf69083f10c47dae064d14dbd9d52b452abfd6
David Sterba <dsterba@suse.com>
2020-07-02 10:54:11 +0200

    btrfs: precalculate checksums per leaf once
    btrfs_csum_bytes_to_leaves shows up in system profiles, which makes it a
    candidate for optimizations. After the 64bit division has been replaced
    by shift, there's still a calculation done each time the function is
    called: checksums per leaf.
    
    As this is a constanat value for the entire filesystem lifetime, we
    can calculate it once at mount time and reuse. This also allows to
    reduce the division to 64bit/32bit as we know the constant will always
    fit to 32bit type.
    
    Replace the open-coded rounding up with a macro that internally handles
    the 64bit division and as it's now a short function, make it static
    inline (slight code increase, slight stack usage reduction).
    
    Reviewed-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/ctree.h
+++ fs/btrfs/ctree.h
@@ -934,6 +934,7 @@ struct btrfs_fs_info {
 	u32 sectorsize;
 	u32 sectorsize_bits;
 	u32 csum_size;
+	u32 csums_per_leaf;
 	u32 stripesize;
 
 	/* Block groups and devices containing active swapfiles. */
@@ -2521,7 +2522,17 @@ int btrfs_get_extent_inline_ref_type(const struct extent_buffer *eb,
 				     enum btrfs_inline_ref_type is_data);
 u64 hash_extent_data_ref(u64 root_objectid, u64 owner, u64 offset);
 
-u64 btrfs_csum_bytes_to_leaves(struct btrfs_fs_info *fs_info, u64 csum_bytes);
+/*
+ * Take the number of bytes to be checksummmed and figure out how many leaves
+ * it would require to store the csums for that many bytes.
+ */
+static inline u64 btrfs_csum_bytes_to_leaves(
+			const struct btrfs_fs_info *fs_info, u64 csum_bytes)
+{
+	const u64 num_csums = csum_bytes >> fs_info->sectorsize_bits;
+
+	return DIV_ROUND_UP_ULL(num_csums, fs_info->csums_per_leaf);
+}
 
 /*
  * Use this if we would be adding new items, as we could split nodes as we cow
