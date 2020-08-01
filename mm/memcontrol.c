471e331c28153de8992869d26898182358892397
Yafang Shao <laoar.shao@gmail.com>
2020-08-01 23:12:16 +1000

    mm-oom-make-the-calculation-of-oom-badness-more-accurate-v3
    - fix the type of variable 'point' in oom_evaluate_task()
    - initialize oom_control->chosen_points in select_bad_process() per Michal
    - update the comment in proc_oom_score() per Michal
    
    Link: http://lkml.kernel.org/r/1594396651-9931-1-git-send-email-laoar.shao@gmail.com
    Signed-off-by: Yafang Shao <laoar.shao@gmail.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Qian Cai <cai@lca.pw>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -1668,7 +1668,6 @@ static bool mem_cgroup_out_of_memory(struct mem_cgroup *memcg, gfp_t gfp_mask,
 		.memcg = memcg,
 		.gfp_mask = gfp_mask,
 		.order = order,
-		.chosen_points = LONG_MIN,
 	};
 	bool ret = true;
 
