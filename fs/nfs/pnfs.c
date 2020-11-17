3f8949cdf458c5d68c867308a3ab88d7d7f56034
Gustavo A. R. Silva <gustavoars@kernel.org>
2020-11-16 19:46:45 -0600

    nfs: Fix fall-through warnings for Clang
    In preparation to enable -Wimplicit-fallthrough for Clang, fix multiple
    warnings by explicitly add multiple break/goto/return/fallthrough
    statements instead of letting the code fall through to the next case.
    
    Link: https://github.com/KSPP/linux/issues/115
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>

--- fs/nfs/pnfs.c
+++ fs/nfs/pnfs.c
@@ -2824,6 +2824,7 @@ pnfs_do_write(struct nfs_pageio_descriptor *desc,
 	switch (trypnfs) {
 	case PNFS_NOT_ATTEMPTED:
 		pnfs_write_through_mds(desc, hdr);
+		break;
 	case PNFS_ATTEMPTED:
 		break;
 	case PNFS_TRY_AGAIN:
@@ -2968,6 +2969,7 @@ pnfs_do_read(struct nfs_pageio_descriptor *desc, struct nfs_pgio_header *hdr)
 	switch (trypnfs) {
 	case PNFS_NOT_ATTEMPTED:
 		pnfs_read_through_mds(desc, hdr);
+		break;
 	case PNFS_ATTEMPTED:
 		break;
 	case PNFS_TRY_AGAIN:
